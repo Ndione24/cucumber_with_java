6bfd39b8f1dab6f5d4b8a6a474839725f6ef1588
Mouhamed <ndionebaba@gmail.com>
2023-10-08 16:15:53 +0200

    Add Util Constants

---
+++ src/main/java/util/Constants.java
@@ -0,0 +1,9 @@
+package util;
+
+public class Constants {
+
+    public static final String FIRST_NAME = "First Name";
+    public static final String LAST_NAME = "Last Name";
+    public static final String STUDENT_ID = "Student ID";
+    public static final String DATE_OF_BIRTH = "Date of Birth";
+}
